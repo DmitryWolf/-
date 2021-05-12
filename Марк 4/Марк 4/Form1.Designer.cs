3a2969ffc7c9752e02743c3c392c6a54db7a87d4
DmitryWolf <68951814+DmitryWolf@users.noreply.github.com>
2021-05-12 23:26:45 +0300

    Add files via upload

---
+++ Марк 4/Марк 4/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿
+namespace Марк_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label2.Location = new System.Drawing.Point(9, 433);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label3.Location = new System.Drawing.Point(167, 433);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "label3";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(79, 405);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Play";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(79, 332);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Open";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(12, 471);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(201, 56);
+            this.trackBar1.TabIndex = 5;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(714, 376);
+            this.trackBar2.Maximum = 100;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar2.Size = new System.Drawing.Size(56, 151);
+            this.trackBar2.TabIndex = 6;
+            this.trackBar2.Value = 50;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(79, 267);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Convert";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 24;
+            this.listBox1.Location = new System.Drawing.Point(12, 545);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(758, 196);
+            this.listBox1.TabIndex = 8;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.label1);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(295, 33);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(317, 147);
+            this.flowLayoutPanel1.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Марк_4.Properties.Resources.скрипичный_ключ;
+            this.ClientSize = new System.Drawing.Size(782, 753);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(800, 800);
+            this.Name = "Form1";
+            this.Text = "Редактор";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+    }
+}
+
---
+++ Марк 4/Марк 4/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DAudio;
+using WindowsInput;
+using WindowsInput.Native;
+using System.Diagnostics;
+
+namespace Марк_4
+{
+    public partial class Form1 : Form
+    {
+        InputSimulator sim = new InputSimulator();
+        private AudioPlayer Player;
+        string str = "";
+        List<string> arr = new List<string>();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            Player = new AudioPlayer();
+            Player.PlayingStatusChanged += (s, e) => button1.Text = e == Status.Playing ? "Pause" : "Play";
+            Player.AudioSelected += (s, e) =>
+            {
+                trackBar1.Maximum = (int)e.Duration;
+                label1.Text = e.Name;
+                label3.Text = e.DurationTime.ToString(@"mm\:ss");
+                listBox1.SelectedItem = e.Name;
+            };
+            Player.ProgressChanged += (s, e) =>
+            {
+                trackBar1.Value = (int)e; // ошибка при воспроизведении midi
+                label2.Text = ((AudioPlayer)s).PositionTime.ToString(@"mm\:ss");
+            };
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Location = new Point(flowLayoutPanel1.Location.X + flowLayoutPanel1.Width / 2, flowLayoutPanel1.Location.Y + flowLayoutPanel1.Height / 2);
+            flowLayoutPanel1.BackColor = Color.Transparent;
+            label1.Text = "";
+            label1.BackColor = Color.Transparent;
+            label1.ForeColor = Color.White;
+
+            label2.Text = "";
+            label2.BackColor = Color.Transparent;
+            label2.ForeColor = Color.White;
+
+            label3.Text = "";
+            label3.BackColor = Color.Transparent;
+            label3.ForeColor = Color.White;
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (((Button)sender).Text == "Play")
+                Player.Play();
+            else if (((Button)sender).Text == "Pause")
+                Player.Pause();
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog dialog = new OpenFileDialog()
+            {
+                Multiselect = true,
+                Filter = "Audio Files|*.wav;*mid;"
+            }
+            )
+            {
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    for (int i = 0; i < dialog.FileNames.Count(); i++)
+                    {
+                            arr.Add(dialog.FileNames[i]);
+                    }
+
+                    str = dialog.FileName;
+                    Player.LoadAudio(dialog.FileNames);
+                    listBox1.Items.Clear();
+
+                    //listBox1.Items.AddRange(Player.Playlist);
+
+                    for (int i = 0; i < arr.Count(); i++)
+                    {
+                        //if (listBox1.FindString(arr[i]) == -1)
+                            listBox1.Items.Add(arr[i]);
+                    }
+                    //arr.Clear();
+                }
+            }
+        }
+
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (((ListBox)sender).SelectedItem == null)
+                return;
+            Player.SelectAudio(((ListBox)sender).SelectedIndex);
+            str = listBox1.SelectedItem.ToString();
+        }
+
+       
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (str != "")
+            {
+                if (str[str.Length - 1] != 'v' || str[str.Length - 2] != 'a' || str[str.Length - 3] != 'w')
+                {
+                    MessageBox.Show("Вы пытаетесь перевести перевести " + str[str.Length - 3] + str[str.Length - 2] + str[str.Length - 1] + " файл. Выберите wav файл", "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    Process.Start("C:/Windows/system32/cmd.exe");
+                    sim.Keyboard.Sleep(1000);
+                    sim.Keyboard.TextEntry("cd C:/Users/Hunte/PycharmProjects/pythonProject8");
+                    sim.Keyboard.KeyPress(VirtualKeyCode.RETURN);
+                    sim.Keyboard.TextEntry("python main.py");
+                    sim.Keyboard.KeyPress(VirtualKeyCode.RETURN);
+                    sim.Keyboard.TextEntry(str);
+                    sim.Keyboard.KeyPress(VirtualKeyCode.RETURN);
+                    sim.Keyboard.TextEntry("exit");
+                    sim.Keyboard.KeyPress(VirtualKeyCode.RETURN);
+                }
+            }
+        }
+
+
+        private void trackBar1_Scroll(object sender, EventArgs e) => Player.Position = ((TrackBar)sender).Value;
+
+        private void trackBar2_Scroll(object sender, EventArgs e) => Player.Volume = ((TrackBar)sender).Value;
+    }
+}
